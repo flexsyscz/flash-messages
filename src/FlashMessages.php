17010767409b835f910a1c2e2aefecc27bda1805
Václav Lukeš <vasek@flexsys.cz>
2023-08-09 15:54:25 +0200

    Initial commit

---
+++ src/FlashMessages.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexsyscz\FlashMessages;
+
+use Nette\HtmlStringable;
+use stdClass;
+
+
+trait FlashMessages
+{
+	/**
+	 * @param HtmlStringable|stdClass|string|Message $message
+	 * @param string $type
+	 * @return stdClass
+	 */
+	public function flashMessage($message, string $type = 'info'): stdClass
+	{
+		if (!$message instanceof Message) {
+			throw new InvalidArgumentException(sprintf('Argument $message must be instance of %s.', Message::class));
+		}
+
+		if (!MessageType::from($type)) {
+			throw new InvalidArgumentException(sprintf('Argument $type is not valid constant of %s class.', MessageType::class));
+		}
+
+		$presenter = $this->getPresenter();
+		if ($presenter->isAjax()) {
+			$presenter->redrawControl('flashes');
+		}
+
+		return parent::flashMessage($message, $type);
+	}
+
+
+	public function flashInfo(string $message, string $caption = null): stdClass
+	{
+		if (method_exists($this, 'translate')) {
+			$message = $this->translate($message);
+		}
+
+		$message = new Message($message, $caption);
+		return $this->flashMessage($message, MessageType::Info->value);
+	}
+
+
+	public function flashWarning(string $message, string $caption = null): stdClass
+	{
+		if (method_exists($this, 'translate')) {
+			$message = $this->translate($message);
+		}
+
+		$message = new Message($message, $caption);
+		return $this->flashMessage($message, MessageType::Warning->value);
+	}
+
+
+	public function flashError(string $message, string $caption = null): \stdClass
+	{
+		if (method_exists($this, 'translate')) {
+			$message = $this->translate($message);
+		}
+
+		$message = new Message($message, $caption);
+		return $this->flashMessage($message, MessageType::Error->value);
+	}
+
+
+	public function flashSuccess(string $message, string $caption = null): \stdClass
+	{
+		if (method_exists($this, 'translate')) {
+			$message = $this->translate($message);
+		}
+
+		$message = new Message($message, $caption);
+		return $this->flashMessage($message, MessageType::Success->value);
+	}
+}
